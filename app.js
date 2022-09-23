ed726398db881a7b19a1efb56608797948740719
a99os <abbosashirboev@gmail.com>
2022-09-23 14:55:39 +0500

    birinchi versiya

---
+++ app.js
@@ -0,0 +1,35 @@
+let players1 = ["Abbos", "Omadbek", "Nomonjon", "Azimxoja"];
+let players2 = ["Murtazoxon", "Muhammadyahyo", "Sanjar Ali", "Mirabdulloh"];
+let players3 = ["Botirjon", "Abdulaziz"];
+
+let kattaDiv = document.querySelector(".umumiy");
+let count = 1;
+function sleep() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 1500);
+  });
+}
+async function ran(players) {
+  while (players.length) {
+    await sleep();
+    let Ind = Math.floor(Math.random() * players.length);
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    let p = document.createElement("p");
+    div.setAttribute("id", `d${count}`);
+    img.setAttribute("src", "./img/606545.png");
+    p.textContent = players[Ind];
+    count++;
+    div.append(img, p);
+    kattaDiv.append(div);
+    players.splice(Ind, 1);
+  }
+}
+async function bir() {
+  await ran(players1);
+  await ran(players2);
+  await ran(players3);
+}
+bir();
